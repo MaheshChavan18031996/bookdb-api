a75aa2b042b5068fe0aae8d9a2b3447cb1246f55
Mahesh Sanjay Chavan <88845892+MaheshChavan18031996@users.noreply.github.com>
2024-07-10 14:14:31 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-router.get('/', bookController.getAllBooks);
+router.post('/getData', bookController.getAllBooks);
 router.post('/', bookController.createBook);
 router.post('/update', bookController.updateBook);
 router.post('/delete', bookController.deleteBook);
